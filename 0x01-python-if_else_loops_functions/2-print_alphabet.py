ffafa5a6f2f46510d54590d5844e0a574b5bb4d5
Amaara96 <amara.alex96@gmail.com>
2024-07-17 10:48:27 +0000

    indention

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# A program that prints all the alphabets in lowercase 
+# A program that prints all the alphabets in lowercase
 
 for i in range(97, 123):
-	print("{}".format(chr(i)), end='')
+    print("{}".format(chr(i)), end='')
